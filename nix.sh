1a7ebd12711dffc39dc640afc15c7adb1ecee02a
Jellu <105118442+Debilesque@users.noreply.github.com>
2024-07-25 17:23:46 -0600

    still working on the scripts

--- nix.sh
+++ nix.sh
@@ -5,6 +5,8 @@ REPODIR="~/repos"
 echo $PWD
 echo $ACCOUNT
 
+mkdir $REPODIR
+
 ## clone lemonpie, scripts, emacs, nvim, dotfiles
 git clone ${ACCOUNT}/dotfiles ${REPODIR}
 git clone ${ACCOUNT}/scripts ${REPODIR}
@@ -13,11 +15,16 @@ git clone ${ACCOUNT}/emacs ${REPODIR}
 
 ## ---
 
-# sudo ln -s $PWD/bash/etc/bashrc /etc/bash-path.sh
+sudo ln -s ~/repos/dotfiles/nixos/* /etc/nixos/
+sudo nixos-rebuild switch
 
-# sudo ln -s ~/repos/dotfiles/nixos/configuration.nix /etc/nixos/configuration.nix
-# sudo ln -s ~/repos/dotfiles/nixos/desktop.nix /etc/nixos/desktop.nix
+sudo ln -s ~/repos/dotfiles/home-manager/* ~/.config/home-manager/
+home-manager switch
 
-# mkdir ~/{desktop,downloads,templates,public,documents,music,pictures,videos}
+mkdir ~/{desktop,downloads,templates,public,documents,music,pictures,videos,shared}
 
 # xdg-user-dirs-update
+
+mkdir ~/.ssh
+cd ~/.ssh
+ssh-keygen -t rsa -b 4096 -C "github gorvasof@gmail.com" -N "" -f "id_rsa_github"
